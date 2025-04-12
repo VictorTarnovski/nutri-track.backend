37e2745b2aea80b225237186a9e1d1ac49aeacfe
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-04-12 09:49:27 -0300

    refactor: improve OffsetDateTimeRangeEndBeforeStartException message

--- src/main/java/com/nutri_track/domain/exceptions/OffsetDateTimeRangeEndBeforeStartException.java
+++ src/main/java/com/nutri_track/domain/exceptions/OffsetDateTimeRangeEndBeforeStartException.java
@@ -4,6 +4,6 @@ import java.time.OffsetDateTime;
 
 public class OffsetDateTimeRangeEndBeforeStartException extends NutriTrackRuntimeException {
     public OffsetDateTimeRangeEndBeforeStartException(OffsetDateTime start, OffsetDateTime end) {
-        super("Invalid date time range: " + end + " is before " + start);
+        super("Invalid date time range: end:" + end + " is before start:" + start);
     }
 }
