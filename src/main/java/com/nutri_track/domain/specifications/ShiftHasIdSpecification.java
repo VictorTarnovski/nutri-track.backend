ed36e46b6fadeed922d7e5043bf8a1b917f725cf
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-04-19 15:48:12 -0300

    refactor: remove unused shift aggregate

--- src/main/java/com/nutri_track/domain/specifications/ShiftHasIdSpecification.java
+++
@@ -1,9 +0,0 @@
-package com.nutri_track.domain.specifications;
-
-import com.nutri_track.domain.entities.Shift;
-
-public class ShiftHasIdSpecification extends HasIdSpecification<Shift> {
-    public ShiftHasIdSpecification(Long shiftId) {
-        super(shiftId);
-    }
-}
--- src/main/java/com/nutri_track/domain/specifications/ShiftIsNotNullSpecification.java
+++
@@ -1,5 +0,0 @@
-package com.nutri_track.domain.specifications;
-
-import com.nutri_track.domain.entities.Shift;
-
-public class ShiftIsNotNullSpecification extends IsNotNullSpecification<Shift> { }
