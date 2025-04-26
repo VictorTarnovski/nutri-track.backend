7a69ab38e88e018d40bc3c1240b892e22dabeb49
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-04-26 16:29:57 -0300

    feat: add more tests to Address

--- src/test/java/com/nutri_track/domain/value_objects/AddressTest.java
+++ src/test/java/com/nutri_track/domain/value_objects/AddressTest.java
@@ -1,6 +1,7 @@
 package com.nutri_track.domain.value_objects;
 
 import com.nutri_track.domain.value_objects.builders.AddressBuilder;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -114,4 +115,72 @@ public class AddressTest {
         });
     }
 
+    @Test
+    void should_create_an_address() {
+        // Arrange
+        var builder = new AddressBuilder()
+                .withLine1("")
+                .withLine2("")
+                .withCity("")
+                .withRegion("")
+                .withPostalCode("")
+                .withCountryCode("");
+
+        // Assert
+        assertDoesNotThrow(builder::build);
+    }
+
+    @Nested
+    class US {
+        @Test
+        void should_create_a_US_address() {
+            // Arrange
+            var builder = new AddressBuilder()
+                    .withLine1("John Doe")
+                    .withLine2("1234 Elm St, Apt 56B")
+                    .withCity("Chicago")
+                    .withRegion("IL")
+                    .withPostalCode("60601")
+                    .withCountryCode("US");
+
+            // Assert
+            assertDoesNotThrow(builder::build);
+        }
+    }
+
+    @Nested
+    class ENG {
+        @Test
+        void should_create_a_ENG_address() {
+            // Arrange
+            var builder = new AddressBuilder()
+                    .withLine1("John Doe")
+                    .withLine2("2045 Royal Road")
+                    .withCity("Chelsea")
+                    .withRegion("London")
+                    .withPostalCode("SW1W 8EG")
+                    .withCountryCode("GB-ENG");
+
+            // Assert
+            assertDoesNotThrow(builder::build);
+        }
+    }
+
+    @Nested
+    class BR {
+        @Test
+        void should_create_a_BR_address() {
+            // Arrange
+            var builder = new AddressBuilder()
+                    .withLine1("John Doe")
+                    .withLine2("Rua Álvaro Correa Alves, 596 - Santa Terezinha")
+                    .withCity("Piracicaba")
+                    .withRegion("SP")
+                    .withPostalCode("13411-025")
+                    .withCountryCode("BR");
+
+            // Assert
+            assertDoesNotThrow(builder::build);
+        }
+    }
 }
